e87ad06e1931c953c03fa6e5b997e0cd2aa47ff7
dinesh-2020-code <74357919+dinesh-2020-code@users.noreply.github.com>
2022-10-14 19:36:33 +0530

    Time: 145 ms (68.24%), Space: 53.6 MB (17.11%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> st; 
+        for (auto i : nums)
+            st.insert(i); 
+        
+        return (st.size() == nums.size()) ? false : true; 
+    }
+};
