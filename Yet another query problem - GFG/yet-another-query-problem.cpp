261172fa9b2a532b9d3cef780578ac092148cee6
dinesh-2020-code <74357919+dinesh-2020-code@users.noreply.github.com>
2023-03-11 11:48:18 +0530

    Added solution - LeetHub

---
+++ Yet another query problem - GFG/yet-another-query-problem.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+    
+    void printVec(vector<int> &v) {
+        
+        for (auto  it : v) {
+            cout << it << " " ; 
+        }
+        
+        cout << endl; 
+    }
+  public:
+  
+    vector<int> solveQueries(int N, int num, vector<int> &A, vector<vector<int>> &Q) {
+        
+        vector<int> ans;
+        
+        vector<int> freqPref(N); 
+        
+        unordered_map<int, int> mp; 
+        
+        for (int i = N - 1; i >= 0; i--) {
+            mp[A[i]]++; 
+            
+            freqPref[i] = mp[A[i]]; 
+            
+        }
+        // printVec(freqPref); 
+        
+        for (int i = 0; i < Q.size(); i++) {
+            int l = Q[i][0], r = Q[i][1], k = Q[i][2]; 
+            
+            int count = 0; 
+            for (int j = l; j <= r; j++) {
+                if (freqPref[j] == k)
+                    count++; 
+            }
+            
+            ans.push_back(count); 
+        }
+        
+        
+        
+        return ans; 
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int num;
+        cin>>num;
+        vector<int> A(N);
+        for(int i=0;i<N;i++){
+            cin>>A[i];
+        }
+        vector<vector<int>> Q(num, vector<int>(3));
+        for(int i=0;i<num;i++){
+            for(int j=0;j<3;j++){
+                cin>>Q[i][j];
+            }
+        }
+        Solution obj;
+        vector<int> res = obj.solveQueries(N, num, A, Q);
+        
+        for(auto ele:res){
+            cout<<ele<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+// } Driver Code Ends
