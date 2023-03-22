daabead76b38d5e0a55289ec1e02240ea7d9f1c1
dinesh-2020-code <74357919+dinesh-2020-code@users.noreply.github.com>
2023-03-22 19:40:24 +0530

    Time: 6 ms (68.10%), Space: 14.1 MB (52.75%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1; 
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2; 
+
+            if (nums[mid] == target)
+                return true; 
+            
+            if (nums[low] == nums[mid] && nums[high] == nums[mid]) 
+                low++, high--; 
+    
+            
+            else if (nums[low] <= nums[mid]) {  //left part is sorted
+                if (target >= nums[low] && target < nums[mid]) 
+                    high = mid - 1; 
+                else 
+                    low = mid + 1; 
+
+            }
+            else { //right part is sorted
+                if (target > nums[mid] && target <= nums[high]) 
+                    low = mid + 1; 
+                else 
+                    high = mid - 1;
+
+            }
+        }
+        return false; 
+
+        // Time complexity
+        // Worst case: O(N)
+        // Average case: O(log N)
+
+    }
+};
