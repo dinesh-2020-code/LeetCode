0dbb01559d68450b8ab39361a7bea2ab7b9e505f
dinesh-2020-code <74357919+dinesh-2020-code@users.noreply.github.com>
2023-03-26 12:24:14 +0530

    Time: 273 ms (50.00%), Space: 78.1 MB (87.50%) - LeetHub

---
+++ 2602-minimum-operations-to-make-all-array-elements-equal/2602-minimum-operations-to-make-all-array-elements-equal.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<long long> minOperations(vector<int>& nums, vector<int>& queries) {
+        
+        sort(nums.begin(), nums.end());
+        int n = nums.size(), m = queries.size(); 
+        
+        vector<long long> ans(m); 
+        vector<long long int> prefSum(n); 
+        prefSum[0] = (long long)(nums[0]); 
+            
+        for (int i = 1; i < n; i++) {
+            prefSum[i] = prefSum[i-1] + (long long) nums[i]; 
+        }
+        
+        for (long long int j = 0; j < m; j++) {
+            long long int x = (long long)queries[j];
+            
+            int i = lower_bound(nums.begin(), nums.end(), x) - nums.begin();
+            
+            long long largerSum = (i-1 >= 0) ? ( prefSum[n-1] - prefSum[i-1]) : prefSum[n-1]; 
+            long long smallerSum = (i-1 >= 0)? prefSum[i-1]: 0; 
+            
+            long long incReq = (1LL * x * i - smallerSum);
+            long long decReq = largerSum - (1LL * x * ((long long)n - i));
+            
+            ans[j] = incReq + decReq; 
+            
+            
+        }
+         
+        return ans; 
+    }
+};
