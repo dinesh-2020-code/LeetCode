72e3b4b44d207e2823c42a2db9fca9060bf0f169
dinesh-2020-code <74357919+dinesh-2020-code@users.noreply.github.com>
2023-04-02 13:12:35 +0530

    Time: 242 ms (76.16%), Space: 97.2 MB (73.07%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -4,13 +4,13 @@ public:
         int n = spells.size(), m = potions.size(); 
         vector<int> ans(n); 
         
-        sort(potions.begin(), potions.end());  
+        sort(potions.begin(), potions.end());    // m log m
         
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {    // n times
             
             int low = 0, high = m -1; 
             int spell = spells[i]; 
-            while (low <= high) {
+            while (low <= high) {         // log m times
                 int mid = low + (high - low) / 2; 
                 
                 if (1LL * spell * potions[mid] >= success) {
@@ -28,3 +28,7 @@ public:
         
     }
 };
+
+/*
+    Overall time: O(m log m) + O(n log m) 
+*/
