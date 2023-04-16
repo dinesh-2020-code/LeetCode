7857d7febeabf7705f056a24f626da8e55e08aaa
dinesh-2020-code <74357919+dinesh-2020-code@users.noreply.github.com>
2023-04-17 01:30:56 +0530

    Time: 2963 ms (5.41%), Space: 399.7 MB (5.42%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
+    int mod = 1e9 + 7;
+    int solve(int t,int ind,vector<string>&words, string &target, vector<vector<int>>&dp){
+        if(t == target.length()){
+            return 1;
+        }
+        if(ind == words[0].size() || words[0].size()-ind < target.size()-t ){
+            return 0;
+        }
+        if(dp[t][ind] != -1)return dp[t][ind];
+        long long take = 0, notTake = 0;
+        for(int i = 0; i<words.size(); i++){
+        if(words[i][ind] == target[t]){
+        take += solve(t+1, ind+1, words, target, dp)%mod;  
+        }
+        }
+        notTake = solve(t, ind+1, words, target, dp)%mod;
+        return dp[t][ind] =  (take + notTake)%mod;  
+    }
     int numWays(vector<string>& words, string target) {
-        int n = words[0].size();
-        int m = target.size();
-        int mod = 1000000007;
-        vector<int> dp(m+1, 0);
-        dp[0] = 1;
-        
-        vector<vector<int>> count(n, vector<int>(26, 0));
-        for (const string& word : words) {
-            for (int i = 0; i < n; i++) {
-                count[i][word[i] - 'a']++;
-            }
-        }
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = m-1; j >= 0; j--) {
-                dp[j+1] += (int)((long)dp[j] * count[i][target[j] - 'a'] % mod);
-                dp[j+1] %= mod;
-            }
-        }
-        
-        return dp[m];
+        vector<vector<int>> dp(1001, vector<int>(1001, -1));
+        return solve(0, 0, words, target, dp);
     }
 };
