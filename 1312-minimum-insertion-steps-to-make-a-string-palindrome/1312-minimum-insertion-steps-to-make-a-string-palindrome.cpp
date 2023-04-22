18eb69551e85db1f7e483312c1f7d1e83f6cad27
dinesh-2020-code <74357919+dinesh-2020-code@users.noreply.github.com>
2023-04-23 01:11:24 +0530

    Time: 95 ms (36.60%), Space: 28.4 MB (50.84%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,40 @@
+class Solution {
+private: 
+    int lcs(string s1, string s2, int m, int n, vector<vector<int>> &dp) {
+        
+        if (m == 0 || n == 0) {
+            return 0; 
+        }
+        if (dp[m][n] != -1)
+            return dp[m][n]; 
+        
+        if (s1[m-1] == s2[n-1]) {
+            return dp[m][n] = 1 + lcs(s1, s2, m-1, n-1, dp); 
+        }
+        
+        int first = lcs(s1, s2, m-1, n, dp); 
+        int second = lcs(s1, s2, m, n-1, dp); 
+        
+        return dp[m][n] = max(first, second); 
+    }
+public:
+    int minInsertions(string s) {
+        int n = s.length(); 
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0)); 
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+        for(int i = 0; i<=n; i++){
+            for(int j = 0; j<=n; j++){
+                if(i == 0 or j == 0)dp[i][j] = 0;
+                else{ 
+                    if(s[i-1] == rev[j-1])dp[i][j] = 1 + dp[i-1][j-1];
+                    else{
+                        dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    }
+                }      
+        }
+        }
+                      
+        return  n - dp[n][n];  
+    }
+};
