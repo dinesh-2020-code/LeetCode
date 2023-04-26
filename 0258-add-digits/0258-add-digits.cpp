6ae9766aac7a54d145d29c6339e1fbdca1d29223
dinesh-2020-code <74357919+dinesh-2020-code@users.noreply.github.com>
2023-04-26 21:19:45 +0530

    Time: 0 ms (100.00%), Space: 6 MB (7.98%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,32 +1,13 @@
 class Solution {
-private:
-    int digitSum(int num) {
-        int ans = 0; 
-        int n = num; 
-        
-        while (n > 0) {
-            
-            int temp = (n % 10);
-            ans += temp; 
-            n /= 10; 
-            
-        }
-        return ans; 
-    }
 public:
     int addDigits(int num) {
-        if(num == 0)return 0;
-        int totalDigits = (log10(num) + 1); 
-       
-        int n = num; 
-        while (totalDigits > 1) {
-            n = digitSum(n); 
-            
-            totalDigits = floor(log10(n) + 1);   
-            
+        if (num == 0)
+            return 0; 
+        
+        if (num % 9 == 0) {
+            return 9; 
         }
         
-        return n; 
-        
+        return (num % 9); 
     }
 };
