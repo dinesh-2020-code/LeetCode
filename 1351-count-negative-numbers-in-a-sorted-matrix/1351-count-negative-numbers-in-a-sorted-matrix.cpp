21722b029bb5fe8a198bfffff76a09ac6e51cb21
dinesh-2020-code <74357919+dinesh-2020-code@users.noreply.github.com>
2023-06-09 00:02:38 +0530

    Time: 19 ms (43.59%), Space: 10.4 MB (71.33%) - LeetHub

---
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        //Brute Force Way N*M time 
+        int ans = 0 ; 
+        
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] < 0)
+                    ans++;
+            }
+        }
+        return ans; 
+    }
+};
